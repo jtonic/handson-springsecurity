a98d0dda39a22b88cf4a4bd56b4e2d57845fb7f2
apazargic <antonel1>
2013-11-04 11:45:55 +0200

    Initial commit

---
+++ src/main/java/ro/handson/springsecurity/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package ro.handson.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import ro.handson.springsecurity.services.UserService;
+import ro.handson.springsecurity.controllers.ApplicationController;
+
+/**
+ * Created by jtonic on 11/4/13.
+ */
+@Configuration
+@ImportResource("classpath:/META-INF/spring/spring-config.xml")
+public class ApplicationConfig {
+
+    @Bean
+    public UserService userService() {
+        return new UserService();
+    }
+
+    @Bean
+    public ApplicationController applicationController() {
+        return new ApplicationController();
+    }
+
+}
